883d838748bd501aa5442e08112047b82893b785
Daylion Templeton <templetondaylion@icloud.com>
2024-11-11 15:36:13 +0000

    added first four films to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
-const test = [
+const test = [{
+        name: "Avatar",
+        value: 2923706026,
+        img: "avatar.jpg",
+        date: 2009,
+    },
     {
-    name: movie1,
-    value: 2000},
+        name: "Avengers: Endgame",
+        value: 2799439100,
+        img: "avengers-endgame.jpg",
+        date: 2019,
+    },
     {
-    name: movie2,
-    value: 1000
-    }]
-
-    
+        name: "Avatar: The Way of Water",
+        value: 2320250281,
+        img: "avatar-way-of-water.jpg",
+        date: 2022,
+    },
+    {
+        name: "Titanic",
+        value: 2264750694,
+        img: "titanic.jpg",
+        date: 1997,
+    }
+];
